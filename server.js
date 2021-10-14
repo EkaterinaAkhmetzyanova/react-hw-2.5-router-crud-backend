22036a67b3848f8f2eca830d2596fcb90707891a
unknown <a.ekaterina323@gmail.com>
2021-10-14 21:18:17 +0300

    add JSON.parse

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ router.get('/posts', async (ctx, next) => {
 });
 
 router.post('/posts', async(ctx, next) => {
-    const {id, content} = ctx.request.body;
+    const {id, content} = JSON.parse(ctx.request.body);
 
     if (id !== 0) {
         posts = posts.map(o => o.id !== id ? o : {...o, content: content});
